7afaf745027744044733b94e867e3aae4f93eace
dev-msj <msj944@naver.com>
2023-01-26 21:24:46 +0900

    template 추가

---
+++ template/src/유형_이름/Daily1.java
@@ -0,0 +1,7 @@
+package greedy;
+
+public class Daily1 {
+    public static void main(String[] args) {
+        // 기출 문제 Solution 작성
+    }
+}
---
+++ template/src/유형_이름/Example1.java
@@ -0,0 +1,7 @@
+package greedy;
+
+public class Example1 {
+    public static void main(String[] args) {
+        // 예제 Solution 작성
+    }
+}
